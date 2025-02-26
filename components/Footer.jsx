8fa65e88c620345bcc48a427ef7213ebae6dd762
uzair rather <uzairrather3147@gmail.com>
2025-02-26 23:27:28 +0530

    change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
        py-14 border-b border-gray-500/30 text-gray-500 bg-[#E6E9F2] ">
         <div className="w-4/5">
           {/* <Image className="w-28 md:w-32" src={assets.logo} alt="logo" /> */}
-          <h2>ecommerence</h2>
+          <h2 className="font-bold text-2xl">E<span>commerence</span></h2>
           <p className="mt-6 text-sm">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
