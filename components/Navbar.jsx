41a7cc9d209f4bd203de64cd4273f037327e2b78
uzair rather <uzairrather3147@gmail.com>
2025-02-26 16:24:58 +0530

    checking

---
+++ components/Navbar.jsx
@@ -0,0 +1,59 @@
+"use client"
+import React from "react";
+import { assets} from "@/assets/assets";
+import Link from "next/link"
+import { useAppContext } from "@/context/AppContext";
+import Image from "next/image";
+
+const Navbar = () => {
+
+  const { isSeller, router } = useAppContext();
+
+  return (
+    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300
+     text-white  bg-gray-400 ">
+      {/* <Image
+        className="cursor-pointer w-28 md:w-32"
+        onClick={() => router.push('/')}
+        src={assets.logo}
+        alt="logo"
+      /> */}
+      <h2 className="font-bold text-2xl">E<span>commerence</span></h2>
+      <div className="flex items-center gap-4 lg:gap-8 max-md:hidden font-bold">
+        <Link href="/" className="hover:text-gray-900 transition">
+          Home
+        </Link>
+        <Link href="/all-products" className="hover:text-gray-900 transition">
+          Shop
+        </Link>
+        <Link href="/" className="hover:text-gray-900 transition">
+          About Us
+        </Link>
+        <Link href="/" className="hover:text-gray-900 transition">
+          Contact
+        </Link>
+
+        {isSeller && <button onClick={() => router.push('/seller')} className="text-xs border px-4 py-1.5 rounded-full">Seller Dashboard</button>}
+
+      </div>
+
+      <ul className="hidden md:flex items-center gap-4 font-bold">
+        <Image className="w-6 h-4 text-white " src={assets.search_icon} alt="search icon" />
+        <button className="flex items-center gap-2 hover:text-gray-900 transition">
+          <Image src={assets.user_icon} alt="user icon" />
+          Account
+        </button>
+      </ul>
+
+      <div className="flex items-center md:hidden gap-3">
+        {isSeller && <button onClick={() => router.push('/seller')} className="text-xs border px-4 py-1.5 rounded-full">Seller Dashboard</button>}
+        <button className="flex items-center gap-2 hover:text-gray-900 transition">
+          <Image src={assets.user_icon} alt="user icon" />
+          Account
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
