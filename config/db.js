c5f5887fb9c8a2b87e02a0d704757c3f9563b950
uzair rather <uzairrather3147@gmail.com>
2025-03-24 14:37:15 +0530

    check

--- config/db.js
+++ config/db.js
@@ -14,7 +14,7 @@ async function connectDB(){
         const opts={
             bufferCommands:false
         }
-        cached.promise=mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then(mongoose=>{
+        cached.promise=mongoose.connect(`${process.env.MONGODB_URI}/ecommerence`,opts).then(mongoose=>{
             return mongoose
         })
     }
