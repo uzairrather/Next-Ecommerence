d67ce8f5a1871dbd619a0b672b653fe38fcdf229
uzair rather <uzairrather3147@gmail.com>
2025-03-25 12:33:27 +0530

    last check2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [...compat.extends("next/core-web-vitals"),
+
+  // Optionally: add other config overrides here
+  {
+    ignores: ["**/node_modules/**", "**/.next/**"],
+  }
+];
 
 export default eslintConfig;
