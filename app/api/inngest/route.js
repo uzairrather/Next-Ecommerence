45e9b4662b14a2bacab9d1f816ed61c6b7b21432
uzair rather <uzairrather3147@gmail.com>
2025-03-25 16:07:06 +0530

    last check3

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,11 +2,11 @@ import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserUpdation,syncUserDeletion} from "@/config/inngest";
 
 // Create an API that serves zero functions
-export const { GET, POST } = serve({
-  client: inngest,
-  functions: [
-    syncUserCreation,
-    // syncUserUpdation,
-    // syncUserDeletion
-  ],
-});
+// export const { GET, POST } = serve({
+//   client: inngest,
+//   functions: [
+//     syncUserCreation,
+//     // syncUserUpdation,
+//     // syncUserDeletion
+//   ],
+// });
