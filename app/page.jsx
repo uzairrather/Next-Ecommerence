f7dcca856d2dcc296784a2b2ca282ade9b8a52b4
uzair rather <uzairrather3147@gmail.com>
2025-03-25 21:25:51 +0530

    last check7

--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,13 @@
 import React from "react";
 import HeaderSlider from "../components/HeaderSlider";
 import HomeProducts from "../components/HomeProducts";
-import Banner from "@/components/Banner";
-import NewsLetter from "@/components/NewsLetter";
-import FeaturedProduct from "@/components/FeaturedProduct";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import BestSeller from "@/components/BestSeller";
-import Carousel from "@/components/Carousel";
+import Banner from "../components/Banner";
+import NewsLetter from "../components/NewsLetter";
+import FeaturedProduct from "../components/FeaturedProduct";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import BestSeller from "../components/BestSeller";
+import Carousel from "../components/Carousel";
 
 
 const Home = () => {
