29f07b758a553da24c765e8fa3400862b1c96293
uzair rather <uzairrather3147@gmail.com>
2025-03-25 21:46:34 +0530

    last check7

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,12 +1,12 @@
-// import { serve } from "inngest/next";
-// import { inngest, syncUserCreation, syncUserUpdation,syncUserDeletion} from "@/config/inngest";
+import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncUserUpdation,syncUserDeletion} from "../../../config/inngest"
 
-// // Create an API that serves zero functions
-// export const { GET, POST, PUT } = serve({
-//   client: inngest,
-//   functions: [
-//     syncUserCreation,
-//     syncUserUpdation,
-//     syncUserDeletion
-//   ],
-// });
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+  ],
+});
--- components/seller/Navbar.jsx
+++ components/seller/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { assets } from '../../assets/assets'
-import Image from 'next/image'
+// import { assets } from '../../assets/assets'
+// import Image from 'next/image'
 import { useAppContext } from '../../context/AppContext'
 
 const Navbar = () => {
