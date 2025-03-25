96363b856872b82905c672434ccc9100538b8c49
uzair rather <uzairrather3147@gmail.com>
2025-03-25 18:35:17 +0530

    last check6

--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,8 @@ import HomeProducts from "@/app/components/HomeProducts";
 import Banner from "@/app/components/Banner";
 import NewsLetter from "@/app/components/NewsLetter";
 import FeaturedProduct from "@/app/components/FeaturedProduct";
-import Navbar from "@/app/components/Navbar";
-import Footer from "@/app/components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import BestSeller from "@/app/components/BestSeller";
 import Carousel from "@/app/components/Carousel";
 
